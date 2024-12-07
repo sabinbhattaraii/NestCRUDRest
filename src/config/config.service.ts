961d8cfb6aeebeb04e9c0d7244c6d24e2d91f572
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-12-07 11:28:47 +0545

    feat:Installed the class validation and transformer to validate the dtos and created the env validation class

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { EnvironmentVariables } from './env-validation';
 
 @Injectable()
 export class ApiConfigService {
