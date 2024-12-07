6cd06d50778af54a9b6faf78ef94a0f1a58e455f
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-12-07 11:53:19 +0545

    feat:Wrote the env validation dto class to validate the env and also imported the validate to congifmodule and config service into the module

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,14 +1,17 @@
 import { Global, Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { validate } from "./env-validation";
+import { ApiConfigService } from "./config.service";
 
 @Global()
 @Module({
     imports : [
         ConfigModule.forRoot({
             isGlobal : true, //Makes the configuration globally available
+            validate,
         }),
     ] , 
-    providers : [],
-    exports : [],
+    providers : [ApiConfigService],
+    exports : [ApiConfigService],
 })
 export class AppConfigModule{}
--- src/config/env-validation.ts
+++ src/config/env-validation.ts
@@ -1,2 +1,84 @@
+import { plainToInstance } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+  validateSync,
+} from 'class-validator';
+
 export class EnvironmentVariables {
+  @IsString()
+  @IsNotEmpty()
+  HOST: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0)
+  @Max(65535)
+  PORT: number;
+
+  @IsString()
+  API_PREFIX: string;
+
+  @IsOptional()
+  @IsString()
+  MODE: string;
+
+  @IsString()
+  API_TITLE: string;
+
+  @IsString()
+  API_DESCRIPTION: string;
+
+  @IsString()
+  API_VERSION: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_HOST: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  @Max(65535)
+  DATABASE_PORT: number;
+
+  @IsString()
+  DATABASE_USER: string;
+
+  @IsString()
+  DATABASE_PASSWORD: string;
+
+  @IsString()
+  DATABASE_NAME: string;
+
+  @IsString()
+  @IsNotEmpty()
+  STRATEGY_ACCESS: string;
+
+  @IsString()
+  @IsNotEmpty()
+  ACCESS_SECRET_KEY_EXPIRE_TIME: string;
+
+  @IsString()
+  @IsNotEmpty()
+  ACCESS_SECRET_KEY: string;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  };
+  return validatedConfig;
 }
