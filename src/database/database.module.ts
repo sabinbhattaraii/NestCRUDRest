e6540a1fc842ed554161cb0655b52ec19b8cccb1
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-12-11 09:53:51 +0545

    feat:Added the code to use and extract the database properties from env and use typeorm in database module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppConfigModule } from 'src/config/config.module';
+import { ApiConfigService } from 'src/config/config.service';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [AppConfigModule],
+      inject: [ApiConfigService],
+      useFactory: (apiConfigService: ApiConfigService) => ({
+        type : 'postgres',
+        host: apiConfigService.getValue('DATABASE_HOST'),
+        port: apiConfigService.getValue('DATABASE_PORT'),
+        username: apiConfigService.getValue('DATABASE_USER'),
+        password: apiConfigService.getValue('DATABASE_PASSWORD'),
+        database: apiConfigService.getValue('DATABASE_NAME'),
+        autoLoadEntities: true, // Automatically load entities
+        synchronize: true, // Synchronize schema (use cautiously in production)
+      }),
+    }),
+  ],
+})
 export class DatabaseModule {}
