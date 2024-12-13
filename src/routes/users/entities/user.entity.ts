faff2e9e255d9fcb6dccbe65d883d9e724d6d1a8
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-12-13 09:38:09 +0545

    feat:Some changes and modification to user entity

--- src/routes/users/entities/user.entity.ts
+++ src/routes/users/entities/user.entity.ts
@@ -1,3 +1,5 @@
+import { Exclude } from 'class-transformer';
+import { IsEmail, Matches, MinLength } from 'class-validator';
 import {
   Column,
   CreateDateColumn,
@@ -16,9 +18,16 @@ export class User {
   name: string;
 
   @Column({ type: 'varchar', unique: true })
+  @IsEmail()
   email: string;
 
-  @Column({ type: 'varchar', length: 255, select: false })
+  @Exclude()
+  @MinLength(6)
+  @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{6,}$/, {
+    message:
+      'Password should be longer than 5 characters, must contain 1 Upper Case Letter, 1 Lower Case Letter, 1 Number and 1 special character.',
+  })
+  @Column({ type: 'varchar', select: false })
   password: string;
 
   @Column({ type : 'boolean' , default: false})
@@ -30,6 +39,6 @@ export class User {
   @UpdateDateColumn()
   updated_on: Date;
 
-  @DeleteDateColumn({ nullable: true })
+  @DeleteDateColumn({ nullable: true , select : false})
   deleted_on: Date;
 }
