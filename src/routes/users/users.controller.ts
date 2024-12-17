18724287729ee033df7538ebda4a34c536cd03f9
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-12-17 09:45:06 +0545

    feat:Added the Sample code for the usercontroller class

--- src/routes/users/users.controller.ts
+++ src/routes/users/users.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('users')
 export class UsersController {}
+  constructor() {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return true ;
+  }
+
+  @Get()
+  findAll() {
+    return true;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return true;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return true;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return true;
+  }
+}
