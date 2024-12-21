55c61215dcbf39c94b1ce246f3fab654aa4d50ca
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-12-21 10:46:55 +0545

    feat:Some coding changes to the datasource file to fill in patter and added the datastore in package to run the migration

--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -3,23 +3,26 @@ import { ApiConfigService } from 'src/config/config.service';
 import { NestFactory } from '@nestjs/core';
 import { AppConfigModule } from 'src/config/config.module';
 
-export async function createDataSource() {
-    const app = await NestFactory.createApplicationContext(AppConfigModule);
-    const apiConfigService = app.get(ApiConfigService);
-  
-    const dataSourceOptions: DataSourceOptions = {
-      type: 'postgres',
-      host: apiConfigService.getValue('POSTGRES_HOST'),
-      port: apiConfigService.getValue('POSTGRES_PORT'),
-      username: apiConfigService.getValue('POSTGRES_USER'),
-      password: apiConfigService.getValue('POSTGRES_PASSWORD'),
-      database: apiConfigService.getValue('POSTGRES_DB'),
-      entities: [__dirname + '/../src/**/*.entity{.ts,.js}'],
-      migrations: [__dirname + '/../migrations/*{.ts,.js}'],
-      migrationsTableName: 'migrations',
-      logging: true, 
-      synchronize: true,
-    }
-  
-    return new DataSource(dataSourceOptions);
-  }
+async function createDataSource() {
+  const app = await NestFactory.createApplicationContext(AppConfigModule);
+  const apiConfigService = app.get(ApiConfigService);
+
+  const dataSourceOptions: DataSourceOptions = {
+    type: 'postgres',
+    host: apiConfigService.getValue('POSTGRES_HOST'),
+    port: apiConfigService.getValue('POSTGRES_PORT'),
+    username: apiConfigService.getValue('POSTGRES_USER'),
+    password: apiConfigService.getValue('POSTGRES_PASSWORD'),
+    database: apiConfigService.getValue('POSTGRES_DB'),
+    entities: [__dirname + '/../src/**/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+    migrationsTableName: 'migrations',
+    logging: true,
+    synchronize: true,
+  };
+  const dataSource = new DataSource(dataSourceOptions);
+  await dataSource.initialize();
+  return dataSource;
+}
+
+export const dataSource = createDataSource();
