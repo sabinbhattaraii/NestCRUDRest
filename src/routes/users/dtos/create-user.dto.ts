2b7179fe9b784e7bdbdb5103f9319f5c4ae13e02
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2025-01-03 09:52:18 +0545

    feat:Updated the create user dto to add the updated fields and added validation to those fields

--- src/routes/users/dtos/create-user.dto.ts
+++ src/routes/users/dtos/create-user.dto.ts
@@ -1,7 +1,11 @@
+import { Gender, UserRole } from '@/common/enums/user-role.enum';
 import { ApiProperty } from '@nestjs/swagger';
 import {
+  IsBoolean,
   IsEmail,
+  IsEnum,
   IsNotEmpty,
+  IsOptional,
   IsString,
   Matches,
   MinLength,
@@ -9,13 +13,34 @@ import {
 
 export class CreateUserDto {
   @ApiProperty({
-    description: 'Enter Your Name Here',
+    description: 'Enter Your First Name Here',
     type: String,
-    example: 'John Doe',
+    example: 'John',
+    required: true,
   })
   @IsNotEmpty()
   @IsString()
-  name: string;
+  firstName: string;
+
+  @ApiProperty({
+    description: 'Enter Your Middle Name Here',
+    type: String,
+    example: 'Prased',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  middleName: string;
+
+  @ApiProperty({
+    description: 'Enter Your Last Name Here',
+    type: String,
+    example: 'Doe',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
 
   @ApiProperty({
     description: 'Enter Your Email Here',
@@ -37,4 +62,83 @@ export class CreateUserDto {
       'Password should be at least 6 characters long, include 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character.',
   })
   password: string;
+
+  @ApiProperty({
+    description: 'Enter Your Gender Here From the Enum',
+    enum: Gender,
+    required: true,
+  })
+  @IsEnum(Gender, { message: 'Gender should be Male, Female, or Other' })
+  @IsNotEmpty()
+  @IsString()
+  gender: Gender;
+
+  @ApiProperty({
+    description: 'Enter Your Address Here',
+    type: String,
+    example: '123 Main St.',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  address: string;
+
+  @ApiProperty({
+    description: 'Enter Your Shipping Address Here',
+    type: String,
+    example: '123 Main St.',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  shippingAddress: string;
+
+  @ApiProperty({
+    description: 'Enter Your Contact Number Here',
+    type: String,
+    example: '1234567890',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  contactNumber: string;
+
+  @ApiProperty({
+    description: 'Enter Your Country Here',
+    type: String,
+    example: 'USA',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  country: string;
+
+  @ApiProperty({
+    description: 'Enter Your City Here',
+    type: String,
+    example: 'New York',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  city: string;
+
+  @ApiProperty({
+    description: 'Enter Your Role Here',
+    enum: UserRole,
+  })
+  @IsEnum(UserRole, { message: 'Role should be User or Admin' })
+  @IsNotEmpty()
+  @IsString()
+  role: UserRole;
+
+  @ApiProperty({
+    description: 'Enter Your Verification Status Here',
+    type: Boolean,
+    example: false,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsBoolean()
+  isVerified: boolean;
 }
