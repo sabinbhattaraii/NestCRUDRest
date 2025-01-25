672d74040fe01f23d295c58ad36583462c669024
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2025-01-25 10:47:09 +0545

    feat:Added the service and service test to the user module and also updated it in the appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { AppConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './routes/users/users.module';
+import { UserService } from './routes/users/user.service';
 
 @Module({
   imports: [DatabaseModule,AppConfigModule ,UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
